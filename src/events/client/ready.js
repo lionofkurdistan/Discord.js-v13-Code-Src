7f3fb1aa5a74033c49006b55a618bfa3d1f04221
Dev Lion Of Kurdistan <76536335+lionofkurdistan@users.noreply.github.com>
2022-10-05 22:31:02 +0300

    Create ready.js

---
+++ src/events/client/ready.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    name: 'ready',
+    once: true,
+
+    /**
+     * @param {Client} client 
+     */
+    async execute(client) {
+        
+        // Puts an activity
+        client.user.setActivity("Lion of kurdistan", {
+            type: "WATCHING",
+            name: "lion pro"
+        });
+        
+        // Send a message on the console
+        console.log(`[LOG] ${client.user.tag} is now online!`);
+        console.log(`[LOG] Bot serving on Ready to serve in ${client.guilds.cache.size} servers`);
+        console.log(`[LOG] Bot serving ${client.users.cache.size} users`);
+    }
+}
